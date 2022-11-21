9be3601724868a8581627271b57a7b94426f16ea
Abdulbaqy <abdelbaqy78@gmail.com>
2022-11-21 21:11:06 +0200

    Initial Commit

---
+++ TestCombine/TestCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCombineApp.swift
+//  TestCombine
+//
+//  Created by abdelbaqy yassin on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
